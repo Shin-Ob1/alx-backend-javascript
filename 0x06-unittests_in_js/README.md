795c1ea046357b30c651f6ae4cf743561a43bcbc
Obinna Ukwa <obikelv@gmail.com>
2024-02-29 01:55:47 -0800

    unittest

---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,1 @@
+This is a readme file for Alx project unittesnt (javascript) in backend specialization
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+	const na = Math.round(a);
+	const bn = Math.round(b);
+	let c = 0;
+	switch (type) {
+	case 'SUM':
+            c = na + bn;
+            break;
+	case 'SUBTRACT':
+            c = na - bn;
+            break;
+	case 'DIVIDE':
+	    if (bn === 0) {
+		c = "Error";
+	    } else {
+		c = na / bn;
+	    }
+	    break;
+	}
+	return c;
+    }
+}
+
+module.exports = Utils;
